4e0665cbcd0f324e07250397d8344cac6dd19023
Olivier Scherrer <pode.fr@gmail.com>
2016-04-08 23:34:17 -0400

    add problem 4

---
+++ resolved/p4.js
@@ -0,0 +1,41 @@
+// https://projecteuler.net/problem=4
+
+// find the largest palindrome from two 3 digits numbers = 999 * 999;
+
+function isPalindrome(number) {
+    var string = number + '';
+
+    return string === string.split('').reverse().join('');
+}
+
+function getPalindromes(max, palindromes) {
+    palindromes = palindromes || [];
+    var variable = max;
+
+    if (max < 2) {
+        return palindromes;
+    }
+
+    while (variable * max--) {
+        if (max && isPalindrome(variable * max)) {
+            palindromes.push(variable * max);
+        }
+    }
+
+    return getPalindromes(variable - 1, palindromes);
+}
+
+function solve(nbOfDigits) {
+    return getPalindromes(Math.pow(10, nbOfDigits) - 1).sort(function (a, b) {
+        if (a > b) {
+            return 1;
+        } else if (a < b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }).pop();
+}
+
+console.log(solve(2)); // should be 9009
+console.log(solve(3)); // should be ?
