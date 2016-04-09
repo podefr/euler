947e6207c46119337721e0bc69097b1301e103ac
Olivier Scherrer <pode.fr@gmail.com>
2016-04-09 00:07:50 -0400

    add problem 7

---
+++ resolved/p7.js
@@ -0,0 +1,25 @@
+// https://projecteuler.net/archives
+
+function buildPrimes(n) {
+    var primes = [2];
+    var i = 3;
+    var isPrime;
+
+    while (primes.length < n) {
+        isPrime = !primes.some(function (prime) {
+            return !(i % prime);
+        });
+
+        if (isPrime) {
+            primes.push(i);
+        }
+
+        i++;
+    }
+
+    return primes.pop();
+}
+
+console.log(buildPrimes(6));
+console.log(buildPrimes(10001));
+
