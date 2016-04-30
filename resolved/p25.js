dbedf1c2434a07bfd281a8045c19f8343953e0d7
Olivier Scherrer <pode.fr@gmail.com>
2016-04-30 01:06:04 -0400

    Add solution for problem 25

---
+++ resolved/p25.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const numbers = require("./lib/numbers");
+
+function findFibonacciUntil(condition, n1, n2, index) {
+    n1 = n1 || 0;
+    n2 = n2 || 1;
+    index = index || 1;
+
+    if (condition(n2, index)) return;
+
+    findFibonacciUntil(condition, n2, numbers.sumBigNumbers(n1+"", n2+""), index + 1);
+}
+
+console.log(findFibonacciUntil(((fibonacci, index) => {
+    if (fibonacci.length >= 1000) {
+        console.log("found number at index", index);
+        return true;
+    }
+})));
